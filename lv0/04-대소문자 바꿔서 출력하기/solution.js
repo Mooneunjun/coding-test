d8404fe7b1f10a6abfdec264c1f19d098d071372
Mooneunjun <mooneunjun@icloud.com>
2024-11-14 00:39:51 +0900

    Add README and solution for lv0 - 대소문자 바꿔서 출력하기

---
+++ lv0/04-대소문자 바꿔서 출력하기/solution.js
@@ -0,0 +1,19 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+  input = [line];
+}).on("close", function () {
+  const str = input[0];
+  const result = [...str]
+    .map((char) =>
+      char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase()
+    )
+    .join("");
+  console.log(result);
+});
