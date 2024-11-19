19bc81c30c22253ab644b8092da0ae8c0a3ef332
Mooneunjun <mooneunjun@icloud.com>
2024-11-20 01:13:32 +0900

    ➕ Add solution for Lv0 - 문자열 겹쳐쓰기 기능 개선 및 테스트 케이스 추가

--- Lv0/10-문자열 겹쳐쓰기/solution.js
+++ Lv0/10-문자열 겹쳐쓰기/solution.js
@@ -1,9 +1,10 @@
 // 문자열 겹쳐쓰가
 function solution(my_string, overwrite_string, s) {
   let answer = [...my_string];
-  answer.splice(s, overwrite_string.length, overwrite_string);
+  answer.splice(s, overwrite_string.length, ...overwrite_string);
 
   return answer.join("");
 }
 
 console.log(solution("He11oWor1d", "lloWorl", 2)); // He11lloWor1d
+console.log(solution("Program29b8UYP", "merS123", 7)); // ProgrammerS123
